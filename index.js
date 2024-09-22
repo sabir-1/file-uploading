94a82053577f2491811cbea52b9f7a259d4e6d67
muhammad <sabirse500@gmail.com>
2024-09-22 03:46:08 -0700

    Chore (removed comment and & removed extra code)

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const path = require("path")
 const express = require("express");
 const multer = require('multer')
-// const engine = require("express")
+
 
 const app = express()
 const PORT = 8000
@@ -26,21 +26,19 @@ app.set("views", path.resolve("./views"))
 //Middleware
 app.use(express.urlencoded({ extended: false }));
 
-
+//Router
 app.get("/", (req, res) => {
     return res.render("home")
 })
 app.post("/upload", upload.single("profileImage"), (req, res) => {
     console.log(req.body)
     console.log(req.file)
-
     return res.redirect("/")
-
 })
   
 
 
 
 
-
+//Local Host
 app.listen(PORT, () => console.log(`Server Started At THIS PORT:8000`))
